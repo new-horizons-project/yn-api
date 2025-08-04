46070753b3e70907bf16bc7f86937ed76f795346
iassert <dimakuzn42@gmail.com>
2025-08-04 11:56:58 +0000

    Basic JWT auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,60 @@
-from datetime import datetime, timedelta, timezone
-from typing import Annotated
+#from datetime import datetime, timedelta, timezone
+#from typing import Annotated
 
-import jwt
-from fastapi import Depends, HTTPException, status, APIRouter
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
-from pydantic import BaseModel
+#import jwt
+from fastapi import Depends, APIRouter #HTTPException, status, 
+from fastapi.security import (
+	OAuth2PasswordRequestForm, #OAuth2PasswordBearer,
+	HTTPBearer, HTTPAuthorizationCredentials
+)
+#from jwt.exceptions import InvalidTokenError
+#from passlib.context import CryptContext
+#from pydantic import BaseModel
 
-from ..config import settings
+#from ..config import settings
+from ..schema.token import Token, AccessToken, RefreshToken
+from ..utils.security import (
+	create_access_token, create_refresh_token, validate_refresh_token
+)
 
 router = APIRouter(prefix="/auth")
+security = HTTPBearer()
+
+
+@router.post("/login", response_model=Token)
+def login(form: OAuth2PasswordRequestForm = Depends()) -> Token:
+	# TODO check with database
+	#raise HTTPException(
+	#	status_code=status.HTTP_400_BAD_REQUEST, 
+	#	detail="Incorrect username or password"
+	#)
+
+	access_token = create_access_token(sub=form.username)
+	refresh_token = create_refresh_token(sub=form.username)
+
+	return Token(
+		access_token=access_token,
+		refresh_token=refresh_token,
+		token_type="bearer",
+	)
+
+@router.post("/refresh/access", response_model=AccessToken)
+def refresh_access_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> AccessToken:
+    payload = validate_refresh_token(credentials)
+    new_access_token = create_access_token(**payload)
+
+    return AccessToken(
+        access_token=new_access_token,
+        token_type="bearer"
+    )
+
+
+@router.post("/refresh/refresh", response_model=RefreshToken)
+def refresh_refresh_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> RefreshToken:
+    payload = validate_refresh_token(credentials)
+    new_refresh_token = create_refresh_token(**payload)
+    
+    return RefreshToken(
+        refresh_token=new_refresh_token,
+        token_type="bearer"
+    )
---
+++ app/utils/security.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta, timezone
+from typing import Any, Dict
+
+import jwt
+from fastapi import HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+
+from ..config import settings 
+
+
+def create_access_token(**kwargs: Any) -> str:
+	kwargs.update(type = "access")
+	return create_token(kwargs, settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+def create_refresh_token(**kwargs: dict) -> str:
+	kwargs.update(type = "refresh")
+
+	return create_token(kwargs, settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+
+def create_token(data: dict, exp_minutes: int) -> str:
+	expiration = datetime.now(timezone.utc) + timedelta(minutes = exp_minutes)
+	payload = data.copy()
+	payload.update({"exp": expiration})
+
+	return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_token(token: str) -> Dict[str, Any]:
+	try:
+		return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+	except ExpiredSignatureError:
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid expired")
+	except InvalidTokenError:
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+def validate_refresh_token(credentials: HTTPAuthorizationCredentials) -> Dict[str, Any]:
+    token = credentials.credentials
+    payload = decode_token(token)
+
+    if payload.get("type") != "refresh":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token type"
+        )
+
+    return payload
