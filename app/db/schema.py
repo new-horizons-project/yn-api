5e554b8f265d480aeb8c7f4370dc5370efc89528
at-elcapitan <gameforceman1808@gmail.com>
2025-10-21 06:17:56 +0300

    add: db schema change

--- app/db/schema.py
+++ app/db/schema.py
@@ -20,7 +20,7 @@ class Base(DeclarativeBase):
 class User(Base):
 	__tablename__ = "users"
 
-	id                       : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+	id                       : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 	username                 : Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
 	password_hash            : Mapped[str] = mapped_column(String(255), nullable=False)
 	registration_date        : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
@@ -28,12 +28,13 @@ class User(Base):
 	is_disabled              : Mapped[bool] = mapped_column(Boolean, default=False)
 	force_password_change    : Mapped[bool] = mapped_column(Boolean, default=False)
 
-	topic                : Mapped[list[Topic]] = relationship(back_populates="creator", cascade="all, delete-orphan")
-	tokens               : Mapped[list[JWT_Token]] = relationship(back_populates="user", cascade="all, delete-orphan")
-	topic_translations   : Mapped[TopicTranslation] = relationship(back_populates="user")
-	audit_log            : Mapped[list[Audit]] = relationship(back_populates="user")
-	media_owner          : Mapped[list[MediaObject]] = relationship(back_populates="user_uploader", foreign_keys="[MediaObject.uploaded_by_user_id]")
-	media_uploader       : Mapped[list[MediaObject]] = relationship(back_populates="user_owner", foreign_keys="[MediaObject.used_user_id]")
+	topic                        : Mapped[list[Topic]] = relationship(back_populates="creator", cascade="all, delete-orphan")
+	tokens                       : Mapped[list[JWT_Token]] = relationship(back_populates="user", cascade="all, delete-orphan")
+	topic_translations           : Mapped[TopicTranslation] = relationship(back_populates="user")
+	topic_translations_editor    : Mapped[TopicTranslation] = relationship(back_populates="user_last_editor")
+	audit_log                    : Mapped[list[Audit]] = relationship(back_populates="user")
+	media_owner                  : Mapped[list[MediaObject]] = relationship(back_populates="user_uploader", foreign_keys="[MediaObject.uploaded_by_user_id]")
+	media_uploader               : Mapped[list[MediaObject]] = relationship(back_populates="user_owner", foreign_keys="[MediaObject.used_user_id]")
 
 
 class JWT_Token(Base):
@@ -41,7 +42,7 @@ class JWT_Token(Base):
 
 	id               : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
 	token            : Mapped[str] = mapped_column(Text, nullable=False)
-	user_id          : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+	user_id          : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
 	created          : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc))
 	last_used        : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
 	device_name      : Mapped[str] = mapped_column(String(100), nullable=False)
@@ -70,7 +71,8 @@ class Topic(Base):
 	name_hash          : Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
 	created_at         : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc))
 	edited_at          : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
-	creator_user_id    : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"))
+	imported           : Mapped[bool] = mapped_column(Boolean, nullable=False)
+	creator_user_id    : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"))
 	cover_image_id     : Mapped[Optional[int]] = mapped_column(ForeignKey("media_object.id", ondelete="SET NULL"), nullable=True)
 
 	creator: Mapped[User] = relationship(back_populates="topic")
@@ -96,12 +98,15 @@ class TopicTranslation(Base):
 	id               : Mapped[int] = mapped_column(primary_key=True)
 	translation_id   : Mapped[int] = mapped_column(ForeignKey("translations.id", ondelete="CASCADE"), nullable=False)
 	topic_id         : Mapped[int] = mapped_column(ForeignKey("topic.id", ondelete="CASCADE"), nullable=False)
-	creator_user_id  : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"))
+	creator_user_id  : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"))
 	parse_mode       : Mapped[ParseMode] = mapped_column(SqlEnum(ParseMode, native_enum=False), nullable=False)
+	last_edited_by   : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"))
 	text             : Mapped[str] = mapped_column(Text, nullable=False)
+	first            : Mapped[bool] = mapped_column(Boolean, nullable=False)
 
 	translation: Mapped[Translation] = relationship(back_populates="topic_translations")
 	user: Mapped[User] = relationship(back_populates="topic_translations")
+	user_last_editor: Mapped[User] = relationship(back_populates="topic_translations_editor")
 	topic: Mapped[Topic] = relationship(back_populates="translations")
 
 
@@ -161,9 +166,9 @@ class TopicLink(Base):
 class Audit(Base):
 	__tablename__ = "audit"
 
-	id               : Mapped[int] = mapped_column(primary_key=True)
+	id               : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 	action_type      : Mapped[ActionType] = mapped_column(SqlEnum(ActionType, native_enum=False), nullable=False)
-	user_id          : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
+	user_id          : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
 	timestamp        : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
 	reason           : Mapped[str] = mapped_column(Text, nullable=True)
 
@@ -174,7 +179,7 @@ class Audit(Base):
 class AuditEffectedObject(Base):
 	__tablename__ = "audit_effected_objects"
 
-	id               : Mapped[int] = mapped_column(primary_key=True)
+	id               : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 	audit_id         : Mapped[int] = mapped_column(ForeignKey("audit.id", ondelete="CASCADE"), nullable=False)
 	object_type      : Mapped[ObjectType] = mapped_column(SqlEnum(ObjectType, native_enum=False), nullable=False)
 	object_id        : Mapped[int] = mapped_column(Integer, nullable=False)
@@ -198,9 +203,9 @@ class MediaObject(Base):
 	has_medium             : Mapped[bool] = mapped_column(Boolean, default=False)
 	has_large              : Mapped[bool] = mapped_column(Boolean, default=False)
 
-	uploaded_by_user_id    : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=False)
+	uploaded_by_user_id    : Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=False)
 	used_topic_id          : Mapped[Optional[int]] = mapped_column(ForeignKey("topic.id", ondelete="SET NULL"), nullable=True)
-	used_user_id           : Mapped[Optional[int]] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
+	used_user_id           : Mapped[Optional[uuid.UUID]] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
 
 	user_owner       : Mapped[Optional[User]] = relationship(back_populates="media_uploader", foreign_keys=[used_user_id])
 	user_uploader    : Mapped[User] = relationship(back_populates="media_owner", foreign_keys=[uploaded_by_user_id])
@@ -226,7 +231,7 @@ class ApplicationParameter(Base):
 class APValue(Base):
 	__tablename__ = "ap_value"
 
-	id       : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+	id       : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 	value    : Mapped[str] = mapped_column(Text, nullable=True)
 	override : Mapped[bool] = mapped_column(Boolean, default=False)
 	ap_id    : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),
