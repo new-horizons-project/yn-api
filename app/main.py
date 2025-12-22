e5b048c06de016ad646faa8543754aff428d471a
at-elcapitan <gameforceman1808@gmail.com>
2025-12-22 07:52:39 +0200

    reallign

--- app/main.py
+++ app/main.py
@@ -29,44 +29,44 @@ async def init_config(db: AsyncSession):
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print(f"""                    
-    {Fore.BLUE + Style.BRIGHT}                       ##
-    {Fore.BLUE + Style.BRIGHT}                      ###
-    {Fore.BLUE + Style.BRIGHT}                **## *###
-    {Fore.BLUE + Style.BRIGHT}              ++**# **##         {Fore.CYAN + Style.BRIGHT}New Horizons{Style.RESET_ALL}
-    {Fore.BLUE + Style.BRIGHT}        =-----===+ ***#          {Fore.CYAN + Style.BRIGHT}API Codename {Style.RESET_ALL}{Fore.YELLOW}Yoshino Niku{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}   =-----== = ====+++**                    
-    {Fore.BLUE + Style.BRIGHT}  ---      ===++ +*******#       {Style.BRIGHT + Fore.CYAN}Python          : {Style.RESET_ALL}{Fore.YELLOW}{platform.python_version()}{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}  --     --===+ ++***   **##     {Style.BRIGHT + Fore.CYAN}FastAPI version : {Style.RESET_ALL}{Fore.YELLOW}{fastapi.__version__}{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}        ---=== +++***    **#     {Style.BRIGHT + Fore.CYAN}API version     : {Style.RESET_ALL}{Fore.YELLOW}{__version__} {__release_subname__}{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}       --====  +++**     +*      {Style.BRIGHT + Fore.CYAN}OS              : {Style.RESET_ALL}{Fore.YELLOW}{platform.release()}{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}     =---=====++++*   =          {Style.BRIGHT + Fore.CYAN}Started at      : {Style.RESET_ALL}{Fore.YELLOW}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}{Fore.RESET}
-    {Fore.BLUE + Style.BRIGHT}    -----=====++++*
-    {Fore.BLUE + Style.BRIGHT}   ------= ===++++
-    {Fore.BLUE + Style.BRIGHT}    =----  ===+++
-    """)
+	print(f"""                    
+	{Fore.BLUE + Style.BRIGHT}                       ##
+	{Fore.BLUE + Style.BRIGHT}                      ###
+	{Fore.BLUE + Style.BRIGHT}                **## *###
+	{Fore.BLUE + Style.BRIGHT}              ++**# **##         {Fore.CYAN + Style.BRIGHT}New Horizons{Style.RESET_ALL}
+	{Fore.BLUE + Style.BRIGHT}        =-----===+ ***#          {Fore.CYAN + Style.BRIGHT}API Codename {Style.RESET_ALL}{Fore.YELLOW}Yoshino Niku{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}   =-----== = ====+++**                    
+	{Fore.BLUE + Style.BRIGHT}  ---      ===++ +*******#       {Style.BRIGHT + Fore.CYAN}Python          : {Style.RESET_ALL}{Fore.YELLOW}{platform.python_version()}{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}  --     --===+ ++***   **##     {Style.BRIGHT + Fore.CYAN}FastAPI version : {Style.RESET_ALL}{Fore.YELLOW}{fastapi.__version__}{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}        ---=== +++***    **#     {Style.BRIGHT + Fore.CYAN}API version     : {Style.RESET_ALL}{Fore.YELLOW}{__version__} {__release_subname__}{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}       --====  +++**     +*      {Style.BRIGHT + Fore.CYAN}OS              : {Style.RESET_ALL}{Fore.YELLOW}{platform.release()}{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}     =---=====++++*   =          {Style.BRIGHT + Fore.CYAN}Started at      : {Style.RESET_ALL}{Fore.YELLOW}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}{Fore.RESET}
+	{Fore.BLUE + Style.BRIGHT}    -----=====++++*
+	{Fore.BLUE + Style.BRIGHT}   ------= ===++++
+	{Fore.BLUE + Style.BRIGHT}    =----  ===+++
+	""")
 
-    await init_db()
+	await init_db()
 
-    if not os.path.isdir(config.settings.STATIC_MEDIA_FOLDER):
-        os.mkdir(config.settings.STATIC_MEDIA_FOLDER)
+	if not os.path.isdir(config.settings.STATIC_MEDIA_FOLDER):
+		os.mkdir(config.settings.STATIC_MEDIA_FOLDER)
 
-    session_generator = get_session()
-    session = await anext(session_generator)
+	session_generator = get_session()
+	session = await anext(session_generator)
 
-    await init_config(session)
+	await init_config(session)
 
-    try:
-        await ap.init_ap(session)
-		    await init_config(session)
-        await users.create_root_user(session)
-        await topic.create_base_translation(session)
-        await media.init_media(session)
-        await tasks_db.init_tasks(session)
-        await tasks.schedule_tasks(session)
-        yield
-    finally:
-        await session.close()
+	try:
+		await ap.init_ap(session)
+		await init_config(session)
+		await users.create_root_user(session)
+		await topic.create_base_translation(session)
+		await media.init_media(session)
+		await tasks_db.init_tasks(session)
+		await tasks.schedule_tasks(session)
+		yield
+	finally:
+		await session.close()
 
 
 app = FastAPI(title=config.settings.APP_NAME, version=__version__, lifespan=lifespan)
